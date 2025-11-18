4e9f5eb55a8164f5de6bcb7a1bbe3fe73b2cbd5f
Mkdir-arg <farinamatias00@gmail.com>
2025-11-18 15:53:12 +0100

    estable

--- akuna_calc/akuna_calc/settings.py
+++ akuna_calc/akuna_calc/settings.py
@@ -92,6 +92,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Static files finders
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
--- akuna_calc/akuna_calc/urls.py
+++ akuna_calc/akuna_calc/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,8 @@ urlpatterns = [
     path('productos/', include('productos.urls')),
     path('admin-usuarios/', include('usuarios.urls')),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
