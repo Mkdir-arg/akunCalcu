91b11c661f1d420c4987292c0a419f3d358946ba
Mkdir-arg <farinamatias00@gmail.com>
2026-02-03 12:03:25 +0100

    a

--- akuna_calc/productos/admin.py
+++ akuna_calc/productos/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from .models import Producto, Cotizacion, CotizacionItem
+from .models import Producto
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'categoria', 'precio_m2', 'formula', 'activo', 'created_at']
     list_filter = ['categoria', 'formula', 'activo']
     search_fields = ['nombre', 'categoria']
-
-class CotizacionItemInline(admin.TabularInline):
-    model = CotizacionItem
-    extra = 0
-
-@admin.register(Cotizacion)
-class CotizacionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'fecha', 'usuario', 'total_general']
-    list_filter = ['fecha', 'usuario']
-    inlines = [CotizacionItemInline]
