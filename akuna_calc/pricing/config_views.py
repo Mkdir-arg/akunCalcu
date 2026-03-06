98f926fdb572fe42824e102ee6a0fac88662b790
MKDIR <farinamatias00@gmail.com>
2026-03-06 15:43:50 -0300

    asd

--- akuna_calc/pricing/config_views.py
+++ akuna_calc/pricing/config_views.py
@@ -29,7 +29,7 @@ def _next_id(model):
 @login_required
 @user_passes_test(is_staff)
 def extrusoras_config(request):
-    extrusoras = Extrusora.objects.all()
+    extrusoras = Extrusora.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/extrusoras.html', {'extrusoras': extrusoras})
 
 
@@ -74,8 +74,8 @@ def extrusora_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def lineas_config(request):
-    lineas = Linea.objects.select_related('extrusora').all()
-    extrusoras = Extrusora.objects.all()
+    lineas = Linea.objects.select_related('extrusora').exclude(bloqueado='Si')
+    extrusoras = Extrusora.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/lineas.html', {'lineas': lineas, 'extrusoras': extrusoras})
 
 
@@ -120,8 +120,8 @@ def linea_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def productos_config(request):
-    productos = Producto.objects.select_related('linea', 'extrusora').all()
-    lineas = Linea.objects.all()
+    productos = Producto.objects.select_related('linea', 'extrusora').exclude(bloqueado='Si')
+    lineas = Linea.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/productos.html', {'productos': productos, 'lineas': lineas})
 
 
@@ -166,8 +166,8 @@ def producto_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def marcos_config(request):
-    marcos = Marco.objects.select_related('producto').all()
-    productos = Producto.objects.all()
+    marcos = Marco.objects.select_related('producto').exclude(bloqueado='Si')
+    productos = Producto.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/marcos.html', {'marcos': marcos, 'productos': productos})
 
 
@@ -308,8 +308,8 @@ def marco_formulas_guardar(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def hojas_config(request):
-    hojas = Hoja.objects.select_related('marco').all()
-    marcos = Marco.objects.all()
+    hojas = Hoja.objects.select_related('marco').exclude(bloqueado='Si')
+    marcos = Marco.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/hojas.html', {'hojas': hojas, 'marcos': marcos})
 
 
@@ -354,8 +354,8 @@ def hoja_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def interiores_config(request):
-    interiores = Interior.objects.select_related('hoja').all()
-    hojas = Hoja.objects.all()
+    interiores = Interior.objects.select_related('hoja').exclude(bloqueado='Si')
+    hojas = Hoja.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/interiores.html', {'interiores': interiores, 'hojas': hojas})
 
 
@@ -400,7 +400,7 @@ def interior_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def perfiles_config(request):
-    perfiles = Perfil.objects.all()[:200]
+    perfiles = Perfil.objects.exclude(bloqueado='Si')[:200]
     return render(request, 'pricing/config/perfiles.html', {'perfiles': perfiles})
 
 
@@ -443,7 +443,7 @@ def perfil_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def accesorios_config(request):
-    accesorios = Accesorio.objects.all()[:200]
+    accesorios = Accesorio.objects.exclude(bloqueado='Si')[:200]
     return render(request, 'pricing/config/accesorios.html', {'accesorios': accesorios})
 
 
@@ -486,7 +486,7 @@ def accesorio_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def vidrios_config(request):
-    vidrios = Vidrio.objects.all()
+    vidrios = Vidrio.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/vidrios.html', {'vidrios': vidrios})
 
 
@@ -529,7 +529,7 @@ def vidrio_delete(request, pk):
 @login_required
 @user_passes_test(is_staff)
 def tratamientos_config(request):
-    tratamientos = Tratamiento.objects.all()
+    tratamientos = Tratamiento.objects.exclude(bloqueado='Si')
     return render(request, 'pricing/config/tratamientos.html', {'tratamientos': tratamientos})
 
 
