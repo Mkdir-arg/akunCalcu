f466c8fe4727f6983bb7caf34c451278c21bf69e
MKDIR <farinamatias00@gmail.com>
2026-03-12 14:28:47 -0300

    asd

---
+++ akuna_calc/plantillas/migrations/0005_remove_precio_opcionalfabrica.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('plantillas', '0004_opcionalfabrica_formulaopcional'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="ALTER TABLE plantillas_opcionalfabrica DROP COLUMN IF EXISTS precio;",
+            reverse_sql="ALTER TABLE plantillas_opcionalfabrica ADD COLUMN precio DECIMAL(10, 2) DEFAULT 0;"
+        ),
+    ]
