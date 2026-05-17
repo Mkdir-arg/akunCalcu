657b43cd8894311e14b14cc27fadec33de416aa0
MKDIR <farinamatias00@gmail.com>
2026-05-17 23:35:03 +0200

    fix: permitir healthcheck de Railway en produccion

--- akuna_calc/akuna_calc/settings_prod.py
+++ akuna_calc/akuna_calc/settings_prod.py
@@ -3,11 +3,19 @@ import os
 
 DEBUG = False
 
+railway_public_domain = os.environ.get('RAILWAY_PUBLIC_DOMAIN', '').strip()
 _allowed_hosts_env = os.environ.get('ALLOWED_HOSTS', '')
 ALLOWED_HOSTS = [h.strip() for h in _allowed_hosts_env.split(',') if h.strip()]
+if railway_public_domain:
+	ALLOWED_HOSTS.append(railway_public_domain)
+ALLOWED_HOSTS.append('healthcheck.railway.app')
+ALLOWED_HOSTS = list(dict.fromkeys(ALLOWED_HOSTS))
 
 _csrf_trusted_env = os.environ.get('CSRF_TRUSTED_ORIGINS', '')
 CSRF_TRUSTED_ORIGINS = [o.strip() for o in _csrf_trusted_env.split(',') if o.strip()]
+if railway_public_domain:
+	CSRF_TRUSTED_ORIGINS.append(f'https://{railway_public_domain}')
+CSRF_TRUSTED_ORIGINS = list(dict.fromkeys(CSRF_TRUSTED_ORIGINS))
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
